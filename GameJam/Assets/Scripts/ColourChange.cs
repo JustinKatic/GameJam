9d84fb786aa3007338f7a02035fa4ef973b7a3b9
Justin <justinkatic4@gmail.com>
2021-01-27 14:22:49 +1100

    added colour transparent script

--- GameJam/Assets/Scripts/ColourChange.cs
+++ GameJam/Assets/Scripts/ColourChange.cs
@@ -4,21 +4,23 @@ using UnityEngine;
 
 public class ColourChange : MonoBehaviour
 {
-    Color color;
-    Color altColor;
+    private Material mat;
     public float speed;
-    // Start is called before the first frame update
+
     void Start()
     {
-        color = gameObject.GetComponent<Renderer>().material.color;
-        altColor = new Vector4(gameObject.GetComponent<Renderer>().material.color.r, gameObject.GetComponent<Renderer>().material.color.g, gameObject.GetComponent<Renderer>().material.color.b, 0);
-        gameObject.GetComponent<Renderer>().material.color = Color.Lerp(color, altColor, Time.deltaTime * speed);
-
+        mat = gameObject.GetComponent<MeshRenderer>().material;
     }
 
-    // Update is called once per frame
     void Update()
     {
-       // gameObject.GetComponent<Renderer>().material.color = altColor;
+        if (mat.color.a > 0)
+        {
+            Color newColor = mat.color;
+            newColor.a -= Time.deltaTime * speed;
+            mat.color = newColor;
+            gameObject.GetComponent<MeshRenderer>().material = mat;
+        }
     }
+
 }
