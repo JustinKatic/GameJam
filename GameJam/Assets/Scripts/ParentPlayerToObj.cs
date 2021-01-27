206b601d1698a8bc49a63e458bc3141123eba1c5
Justin <justinkatic4@gmail.com>
2021-01-27 13:03:59 +1100

    added player attach to obj script

---
+++ GameJam/Assets/Scripts/ParentPlayerToObj.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+/// <MoveWithPlatforms>
+/// attaches player to the gameObject so that the gameObject moves where the moveing object moves
+public class ParentPlayerToObj : MonoBehaviour
+{
+    private GameObject player;
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    public void AttachPlayerToObj()
+    {
+        player.transform.parent = transform;
+    }
+    public void DetachPlayerFromObj()
+    {
+        player.transform.parent = null;
+    }
+
+}
