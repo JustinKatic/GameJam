23c60d086f80f7154c59f72c71453b5e92f69d02
Justin <justinkatic4@gmail.com>
2021-01-27 20:01:47 +1100

    added door and switch

---
+++ GameJam/Assets/Scripts/DoorBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorBehaviour : MonoBehaviour
+{
+    private bool doorOpened;
+
+    private void Start()
+    {
+        doorOpened = false;
+    }
+    public void OpenDoor()
+    {
+        if (!doorOpened)
+        {
+            transform.Translate(0, 8, 0);
+            doorOpened = true;
+        }
+    }
+    public void CloseDoor()
+    {
+        transform.Translate(0, -8, 0);
+    }
+
+}
