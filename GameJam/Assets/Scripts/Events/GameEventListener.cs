095c51b41eaceb3dc098b0205e8ec3f371b820ff
Suqnier <sl00ka@hotmail.com>
2021-01-27 11:06:06 +1100

    Audio controller prefab setup

---
+++ GameJam/Assets/Scripts/Events/GameEventListener.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+public class GameEventListener : MonoBehaviour
+{
+    public GameEvent[] Event;
+    public UnityEvent Response;
+    private void OnEnable()
+    {
+        for (int i = 0; i < Event.Length; i++)
+            Event[i].RegisterListener(this);
+    }
+    private void OnDisable()
+    {
+        for (int i = 0; i < Event.Length; i++)
+            Event[i].UnregisterListener(this);
+    }
+    public void OnEventRaised() => Response.Invoke();
+}
